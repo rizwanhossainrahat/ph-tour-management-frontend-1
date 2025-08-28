7aa3905a50cfa689f77155868a8288c29625f250
unknown <rizwanhossainrahat400@gmail.com>
2025-08-28 20:32:27 +0600

    part-1 and part-2 added

---
+++ src/routes/index.tsx
@@ -0,0 +1,32 @@
+import App from "@/App";
+import About from "@/pages/About";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import Verify from "@/pages/Verify";
+// import Verify from "@/pages/Verify";
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    Component: App,
+    path: "/",
+    children: [
+      {
+        Component: About,
+        path: "about",
+      },
+    ],
+  },
+  {
+    Component: Login,
+    path: "/login",
+  },
+  {
+    Component: Register,
+    path: "/register",
+  },
+  {
+    Component:Verify,
+    path: "/verify",
+  },
+]);
