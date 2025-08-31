edf5d771b7cb6b6c9c72d592c82f2a29e85c64b9
unknown <rizwanhossainrahat400@gmail.com>
2025-08-31 15:04:54 +0600

    create tour type

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,20 @@
 import App from "@/App";
+import DashboadLayout from "@/components/layout/DashboadLayout";
 import About from "@/pages/About";
+import AddTour from "@/pages/Admin/AddTour";
+import Analytics from "@/pages/Admin/Analytics";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Bookings from "@/pages/User/Bookings";
 import Verify from "@/pages/Verify";
-// import Verify from "@/pages/Verify";
-import { createBrowserRouter } from "react-router";
+import { generateRoutes } from "@/utils/generateRoutes";
+import { createBrowserRouter, Navigate } from "react-router";
+import { adminSidebarItems } from "./adminSidebarItems";
+import { userSidebarItems } from "./userSidebarItems";
+import { withAuth } from "@/utils/withAuth";
+import { role } from "@/constants/role";
+import { TRole } from "@/types";
+import UnAuthorized from "@/pages/UnAuthorized";
 
 export const router = createBrowserRouter([
   {
@@ -12,9 +22,26 @@ export const router = createBrowserRouter([
     path: "/",
     children: [
       {
-        Component: About,
+        Component:withAuth(About),
         path: "about",
       },
+    ],
+  },
+  {
+    Component: withAuth(DashboadLayout,role.admin as TRole),
+    path: "/admin",
+    children:[
+      {index:true,element:<Navigate to="/admin/analytics"/>},
+      ...generateRoutes(adminSidebarItems)
+    ]
+  },
+  {
+    Component:withAuth(DashboadLayout,role.user as TRole),
+    path: "/user",
+    children: [
+       {index:true,element:<Navigate to="/user/bookings"/>},
+      ...generateRoutes(userSidebarItems)
+     
     ],
   },
   {
@@ -28,5 +55,9 @@ export const router = createBrowserRouter([
   {
     Component:Verify,
     path: "/verify",
+  },
+  {
+    Component:UnAuthorized,
+    path: "/unauthorized",
   },
 ]);
